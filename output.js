b0dbf4837eedffdac1fe7ed431db9611e23d2963
DJ <brenek.m@gmail.com>
2018-09-06 19:46:50 +0200

    Update build item (slot) in inventory

---
+++ output.js
@@ -0,0 +1,148 @@
+handlers.helloWorld = function (args, context) {
+    var message = "Hello " + currentPlayerId + "!";
+    log.info(message);
+    var inputValue = null;
+    if (args && args.inputValue)
+        inputValue = args.inputValue;
+    log.debug("helloWorld:", { input: args.inputValue });
+    return { messageValue: message };
+};
+handlers.makeAPICall = function (args, context) {
+    var request = {
+        PlayFabId: currentPlayerId, Statistics: [{
+                StatisticName: "Level",
+                Value: 2
+            }]
+    };
+    var playerStatResult = server.UpdatePlayerStatistics(request);
+};
+handlers.makeHTTPRequest = function (args, context) {
+    var headers = {
+        "X-MyCustomHeader": "Some Value"
+    };
+    var body = {
+        input: args,
+        userId: currentPlayerId,
+        mode: "foobar"
+    };
+    var url = "http://httpbin.org/status/200";
+    var content = JSON.stringify(body);
+    var httpMethod = "post";
+    var contentType = "application/json";
+    var response = http.request(url, httpMethod, content, contentType, headers);
+    return { responseContent: response };
+};
+handlers.handlePlayStreamEventAndProfile = function (args, context) {
+    var psEvent = context.playStreamEvent;
+    var profile = context.playerProfile;
+    var content = JSON.stringify({ user: profile.PlayerId, event: psEvent.EventName });
+    var response = http.request('https://httpbin.org/status/200', 'post', content, 'application/json', null);
+    return { externalAPIResponse: response };
+};
+handlers.completedLevel = function (args, context) {
+    var level = args.levelName;
+    var monstersKilled = args.monstersKilled;
+    var updateUserDataResult = server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            lastLevelCompleted: level
+        }
+    });
+    log.debug("Set lastLevelCompleted for player " + currentPlayerId + " to " + level);
+    var request = {
+        PlayFabId: currentPlayerId, Statistics: [{
+                StatisticName: "level_monster_kills",
+                Value: monstersKilled
+            }]
+    };
+    server.UpdatePlayerStatistics(request);
+    log.debug("Updated level_monster_kills stat for player " + currentPlayerId + " to " + monstersKilled);
+};
+handlers.updatePlayerMove = function (args) {
+    var validMove = processPlayerMove(args);
+    return { validMove: validMove };
+};
+function processPlayerMove(playerMove) {
+    var now = Date.now();
+    var playerMoveCooldownInSeconds = 15;
+    var playerData = server.GetUserInternalData({
+        PlayFabId: currentPlayerId,
+        Keys: ["last_move_timestamp"]
+    });
+    var lastMoveTimestampSetting = playerData.Data["last_move_timestamp"];
+    if (lastMoveTimestampSetting) {
+        var lastMoveTime = Date.parse(lastMoveTimestampSetting.Value);
+        var timeSinceLastMoveInSeconds = (now - lastMoveTime) / 1000;
+        log.debug("lastMoveTime: " + lastMoveTime + " now: " + now + " timeSinceLastMoveInSeconds: " + timeSinceLastMoveInSeconds);
+        if (timeSinceLastMoveInSeconds < playerMoveCooldownInSeconds) {
+            log.error("Invalid move - time since last move: " + timeSinceLastMoveInSeconds + "s less than minimum of " + playerMoveCooldownInSeconds + "s.");
+            return false;
+        }
+    }
+    var playerStats = server.GetPlayerStatistics({
+        PlayFabId: currentPlayerId
+    }).Statistics;
+    var movesMade = 0;
+    for (var i = 0; i < playerStats.length; i++)
+        if (playerStats[i].StatisticName === "")
+            movesMade = playerStats[i].Value;
+    movesMade += 1;
+    var request = {
+        PlayFabId: currentPlayerId, Statistics: [{
+                StatisticName: "movesMade",
+                Value: movesMade
+            }]
+    };
+    server.UpdatePlayerStatistics(request);
+    server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            last_move_timestamp: new Date(now).toUTCString(),
+            last_move: JSON.stringify(playerMove)
+        }
+    });
+    return true;
+}
+handlers.RoomCreated = function (args) {
+    log.debug("Room Created - Game: " + args.GameId + " MaxPlayers: " + args.CreateOptions.MaxPlayers);
+};
+handlers.RoomJoined = function (args) {
+    log.debug("Room Joined - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+};
+handlers.RoomLeft = function (args) {
+    log.debug("Room Left - Game: " + args.GameId + " PlayFabId: " + args.UserId);
+};
+handlers.RoomClosed = function (args) {
+    log.debug("Room Closed - Game: " + args.GameId);
+};
+handlers.RoomPropertyUpdated = function (args) {
+    log.debug("Room Property Updated - Game: " + args.GameId);
+};
+handlers.RoomEventRaised = function (args) {
+    var eventData = args.Data;
+    log.debug("Event Raised - Game: " + args.GameId + " Event Type: " + eventData.eventType);
+    switch (eventData.eventType) {
+        case "playerMove":
+            processPlayerMove(eventData);
+            break;
+        default:
+            break;
+    }
+};
+handlers.UpdateBuildItemState = function (args) {
+    var inventory = server.GetUserInventory({
+        "PlayFabId": currentPlayerId
+    });
+    var item = inventory.Inventory.find(x => x.ItemId == args.ItemName);
+    if (item) {
+        var UpdateCustomDataRequest = {
+            "PlayFabId": currentPlayerId,
+            "ItemInstanceId": item.ItemInstanceId,
+            "Data": {
+                "SlotID": args.SlotID
+            }
+        };
+        server.UpdateUserInventoryItemCustomData(UpdateCustomDataRequest);
+    }
+};
+//# sourceMappingURL=output.js.map
